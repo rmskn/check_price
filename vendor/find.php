f5e83577e8a901ecd468a7af7007537d7748b3b5
rmskn <tremaskin2010@yandex.ru>
2021-06-21 02:19:25 +0300

    Update find.php

--- vendor/find.php
+++ vendor/find.php
@@ -1,4 +1,27 @@
 <?php
+    require_once 'functions.php';
+
     $href = $_POST['findhref'];
-    
+    $ans = check_href($href, 1);
+
+    if ($ans[0]==0)
+    {
+        echo "Данный магазин пока не поддерживается или ссылка некорректна <br/>";
+        die();
+    }
+
+    if ($ans[1]==0)
+    {
+        echo 'Произошла непредвиденная ошибка. Не удается найти товар <br/>';
+        die();
+    }
+
+    echo get_price_by_xpath($href);
+    echo '<br/>';
+    //echo mb_detect_encoding(get_title_by_xpath($href));
+    //echo '<br/>';
+    echo get_title_by_xpath($href);
+    echo '<br/>';
+    echo 'А ещё картинка будет';
+
 ?>

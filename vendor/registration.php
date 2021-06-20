ebaa17dadf64abeb738d9994c75374c73122f5c1
rmskn <tremaskin2010@yandex.ru>
2021-06-21 00:22:58 +0300

    1

---
+++ vendor/registration.php
@@ -0,0 +1,10 @@
+<?php
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $fullname = $_POST['fullname'];
+    $phone = $_POST['phone'];
+    $borndate = $_POST['borndate'];
+    
+    
+?>

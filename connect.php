6f855830d7f050f56a41c888f932a0a2cc41b747
rmskn <tremaskin2010@yandex.ru>
2021-06-20 19:11:46 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,14 @@
 <?php
+ini_set('display_errors', 0);
 
 $connect = mysqli_connect('localhost', 'root', 'root', 'check_price');
 
-    if (!$connect) {
-        die('Ошибка подключения к базе данных!');
-    }
+if (!$connect) {
+    $connect = mysqli_connect('localhost', 'root', 'erosab49', 'check_price');
+}
+
+if (!$connect) {
+    die('Ошибка подключения к базе данных!');
+}
 
 ?>

6df45fa30d0a56fa756d66f1c92415f1c5d50332
S1ngle023 <ilya.balyaikin@yandex.ru>
2021-07-09 20:00:57 +0300

    123

--- pidor.php
+++ pidor.php
@@ -10,19 +10,17 @@
 
         $ans = 0;
 
-        echo 
-
         //Check in DB
         //Return TRUE, if url is correct and DB have needed domen
         //Return FALSE, if url isn't correct or DB haven't domen
-        $shops = mysqli_query($connect, "select url from shops");
+        $shops = mysqli_query($connect, 'select url from shops');
 
         while($row=mysqli_fetch_row($shops))
         {
             echo $row[0];
             echo '</br>';
             
-            if (strripos($href, $row[0]))
+            if (strripos($url, $row[0]))
             {
                 $ans =  1;
                 break;
